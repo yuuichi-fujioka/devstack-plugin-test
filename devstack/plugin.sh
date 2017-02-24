1817d3056e495f3a6bfc27c76ad61d8cd340e50b
Yuuichi Fujioka <fujioka.yuuichi@gmail.com>
2017-02-24 20:03:16 +0900

    remove heat and tacker

--- devstack/plugin.sh
+++ devstack/plugin.sh
@@ -1,5 +1,5 @@
 function configure_log{
-    for f in /etc/nova/nova.conf /etc/glance/glance-api.conf /etc/glance/glance-cache.conf /etc/glance/glance-registry.conf /etc/heat/heat.conf /etc/keystone/keystone.conf /etc/neutron/neutron.conf /etc/tacker/tacker.conf /etc/cinder/cinder.conf
+    for f in /etc/nova/nova.conf /etc/glance/glance-api.conf /etc/glance/glance-cache.conf /etc/glance/glance-registry.conf /etc/keystone/keystone.conf /etc/neutron/neutron.conf
     do
         iniset $f ogging_exception_prefix ''
         iniset $f ogging_debug_format_suffix 'from (pid=%(process)d) %(funcName)s %(pathname)s:%(lineno)d'
@@ -10,10 +10,8 @@ function configure_log{
     iniset /etc/glance/glance-api.conf log_dir /opt/stack/logs/glance
     iniset /etc/glance/glance-cache.conf log_dir /opt/stack/logs/glance
     iniset /etc/glance/glance-registry.conf log_dir /opt/stack/logs/glance
-    iniset /etc/heat/heat.conf log_dir /opt/stack/logs/heat
     iniset /etc/keystone/keystone.conf log_dir /opt/stack/logs/keystone
     iniset /etc/neutron/neutron.conf log_dir /opt/stack/logs/neutron
-    iniset /etc/tacker/tacker.conf log_dir /opt/stack/logs/tacker
     iniset /etc/cinder/cinder.conf log_dir /opt/stack/logs/cinder
 }
 
