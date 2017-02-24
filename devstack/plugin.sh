34d9abc8e88a7d6922691ff3b5903fc9135eb5b0
Yuuichi Fujioka <fujioka.yuuichi@gmail.com>
2017-02-24 19:51:13 +0900

    fix

---
+++ devstack/plugin.sh
@@ -0,0 +1,24 @@
+function configure_log{
+    for f in /etc/nova/nova.conf /etc/glance/glance-api.conf /etc/glance/glance-cache.conf /etc/glance/glance-registry.conf /etc/heat/heat.conf /etc/keystone/keystone.conf /etc/neutron/neutron.conf /etc/tacker/tacker.conf /etc/cinder/cinder.conf
+    do
+        iniset $f ogging_exception_prefix ''
+        iniset $f ogging_debug_format_suffix 'from (pid=%(process)d) %(funcName)s %(pathname)s:%(lineno)d'
+        iniset $f ogging_default_format_string '%(asctime)s.%(msecs)03d %(process)d %(levelname)s %(name)s %(instance)s%(message)s'
+        iniset $f ogging_context_format_string '%(asctime)s.%(msecs)03d %(process)d %(levelname)s %(name)s [%(request_id)s %(project_id)s %(user_id)s] %(instance)s%(message)s'
+    done
+    iniset /etc/nova/nova.conf log_dir /opt/stack/logs/nova
+    iniset /etc/glance/glance-api.conf log_dir /opt/stack/logs/glance
+    iniset /etc/glance/glance-cache.conf log_dir /opt/stack/logs/glance
+    iniset /etc/glance/glance-registry.conf log_dir /opt/stack/logs/glance
+    iniset /etc/heat/heat.conf log_dir /opt/stack/logs/heat
+    iniset /etc/keystone/keystone.conf log_dir /opt/stack/logs/keystone
+    iniset /etc/neutron/neutron.conf log_dir /opt/stack/logs/neutron
+    iniset /etc/tacker/tacker.conf log_dir /opt/stack/logs/tacker
+    iniset /etc/cinder/cinder.conf log_dir /opt/stack/logs/cinder
+}
+
+if [[ "$1" == "stack" && "$2" == "post-config" ]]; then
+    echo_summary "Configuring Log"
+    configure_log
+fi
+
