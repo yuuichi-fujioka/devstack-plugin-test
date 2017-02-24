e4a71b343af4fda09d4039da5a3e8d5a5c457af0
Yuuichi Fujioka <fujioka.yuuichi@gmail.com>
2017-02-24 20:18:49 +0900

    fix function definition

--- devstack/plugin.sh
+++ devstack/plugin.sh
@@ -1,4 +1,4 @@
-function configure_log{
+function configure_log(){
     for f in /etc/nova/nova.conf /etc/glance/glance-api.conf /etc/glance/glance-cache.conf /etc/glance/glance-registry.conf /etc/keystone/keystone.conf /etc/neutron/neutron.conf
     do
         iniset $f ogging_exception_prefix ''
@@ -6,6 +6,7 @@ function configure_log{
         iniset $f ogging_default_format_string '%(asctime)s.%(msecs)03d %(process)d %(levelname)s %(name)s %(instance)s%(message)s'
         iniset $f ogging_context_format_string '%(asctime)s.%(msecs)03d %(process)d %(levelname)s %(name)s [%(request_id)s %(project_id)s %(user_id)s] %(instance)s%(message)s'
     done
+
     iniset /etc/nova/nova.conf log_dir /opt/stack/logs/nova
     iniset /etc/glance/glance-api.conf log_dir /opt/stack/logs/glance
     iniset /etc/glance/glance-cache.conf log_dir /opt/stack/logs/glance
@@ -13,6 +14,7 @@ function configure_log{
     iniset /etc/keystone/keystone.conf log_dir /opt/stack/logs/keystone
     iniset /etc/neutron/neutron.conf log_dir /opt/stack/logs/neutron
     iniset /etc/cinder/cinder.conf log_dir /opt/stack/logs/cinder
+
 }
 
 if [[ "$1" == "stack" && "$2" == "post-config" ]]; then
